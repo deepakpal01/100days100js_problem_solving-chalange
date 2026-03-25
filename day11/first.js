460e04071137f8127dc2432cb151f80d01065941
deepakpal01 <deepakpal1248916@gmail.com>
2026-03-25 23:43:00 +0530

    insertion sort using for loop

--- day11/first.js
+++ day11/first.js
@@ -2,16 +2,33 @@
 
 // basic type of insertion in js
 
-function insertion(arr){
-    for(let i =1;i<arr.length ; i++){
-        let current = i;
-        let j =i-1;
-        while(j>=0 && arr[j]>arr[j+1]){
+// function insertion(arr){
+//     for(let i =1;i<arr.length ; i++){
+//         let current = i;
+//         let j =i-1;
+//         while(j>=0 && arr[j]>arr[j+1]){
+//             arr[j+1]=arr[j];
+//             j--;
+//         }
+//         arr[j+1]=current;
+//     }
+//     return arr;
+// }
+// console.log(insertion([5,4,2,1,3]));
+
+
+// using for loop to write a insertion
+
+
+function insert(arr){
+    for(let i = 1;i<arr.length;i++){
+        let current = arr[i];
+        let j;
+        for(j=i-1;j>=0 && arr[j]>current;j--){
             arr[j+1]=arr[j];
-            j--;
         }
         arr[j+1]=current;
     }
     return arr;
 }
-console.log(insertion([5,4,2,1,3]))
+console.log(insert([5,4,2,1,3]))
